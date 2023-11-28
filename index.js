ac10ed35da347523b38b9cb6690c7565dce6c842
Rehab Sabry <rehab.sabry.000@gmail.com>
2023-11-28 16:01:03 +0200

    Answers of Question 1 & 2

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Answer of first Question
+
+var firstName= prompt('Please Enter Your First Name');
+var lastName = prompt('Please Enter Your Last Name');
+var fullname = firstName +' ' + lastName ;
+var fullName = confirm('Your full name is ' + fullname);
+var age = prompt('Please enter your age');
+alert(`Welcome ${firstName} ${lastName} your age is ${age}.`);
+
+
+// Answer of second question
+
+window.alert("It’s the first release of a calculator that only has a summation feature");
+var firstNumber = prompt ("Please enter first number");
+var secondNumber = prompt ("Please enter second number");
+var result = Number(firstNumber) + Number(secondNumber) ;
+alert(`The total of the two numbers you entered is ${result}`);
